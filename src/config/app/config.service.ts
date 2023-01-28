54a8e527628da53a7d414b4e1a4a4a12ece4a990
Skye Brady <skye@revolancer.com>
2023-01-28 15:54:00 +0000

    Add config

---
+++ src/config/app/config.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+/**
+ * Service dealing with app config based operations.
+ *
+ * @class
+ */
+@Injectable()
+export class AppConfigService {
+  constructor(private configService: ConfigService) {
+    console.log('Config Service Loaded');
+  }
+
+  get name(): string | undefined {
+    return this.configService.get<string>('app.name');
+  }
+  get env(): string | undefined {
+    return this.configService.get<string>('app.env');
+  }
+  get url(): string | undefined {
+    return this.configService.get<string>('app.url');
+  }
+  get port(): number {
+    return Number(this.configService.get<number>('app.port'));
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AppConfigService } from './config/app/config.service';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const appConfig: AppConfigService = app.get(AppConfigService);
+  const logger = new Logger('Bootstrap');
+  logger.log(`Listening on ${appConfig.port}`);
+  await app.listen(appConfig.port);
 }
 bootstrap();
