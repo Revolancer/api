a42cfa9ac07cce8a0ec8fe8050370a9877cec717
Skye Brady <skye@revolancer.com>
2023-05-17 10:58:28 +0000

    user activity

--- src/features/admin/admin.controller.ts
+++ src/features/admin/admin.controller.ts
@@ -11,4 +11,16 @@ export class AdminController {
   async getUserCount() {
     return await this.adminService.countUsers();
   }
+
+  @Get('stats/count_active_users')
+  @UseGuards(AdminAuthGuard)
+  async getActiveUserCount() {
+    const dau = await this.adminService.countDau();
+    const mau = await this.adminService.countMau();
+    return {
+      dau,
+      mau,
+      dauOverMau: (dau / mau).toFixed(3),
+    };
+  }
 }
--- src/features/admin/admin.module.ts
+++ src/features/admin/admin.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
 import { User } from '../users/entities/user.entity';
+import { UserProfile } from '../users/entities/userprofile.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, UserProfile])],
   providers: [AdminService],
   exports: [AdminService],
   controllers: [AdminController],
