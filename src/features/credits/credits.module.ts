a3beebd003d09023588bc08011f3a524fa6493f3
Skye Brady <skye@revolancer.com>
2023-06-07 14:32:24 +0000

    Notify of credits change

--- src/features/credits/credits.module.ts
+++ src/features/credits/credits.module.ts
@@ -4,9 +4,13 @@ import { CreditLogEntry } from './entities/credit-log-entry.entity';
 import { CreditsController } from './credits.controller';
 import { CreditsService } from './credits.service';
 import { UserBalance } from './entities/user-balance.entity';
+import { NotificationsModule } from '../notifications/notifications.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserBalance, CreditLogEntry])],
+  imports: [
+    TypeOrmModule.forFeature([UserBalance, CreditLogEntry]),
+    NotificationsModule,
+  ],
   providers: [CreditsService],
   exports: [CreditsService],
   controllers: [CreditsController],
