b6d390fb5ecaa5365fd9b3db00e4961a02c58f4e
Skye Brady <skye@revolancer.com>
2023-08-02 12:56:23 +0000

    Create admin user

--- src/features/users/entities/seeders/users.seeder.ts
+++ src/features/users/entities/seeders/users.seeder.ts
@@ -5,6 +5,8 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { UserProfile } from '../userprofile.entity';
 import { UserConsent } from '../userconsent.entity';
+import * as argon2 from 'argon2';
+import { UserRole } from '../userrole.entity';
 
 @Injectable()
 export class UsersSeeder implements Seeder {
@@ -15,10 +17,12 @@ export class UsersSeeder implements Seeder {
     private userProfileRepository: Repository<UserProfile>,
     @InjectRepository(UserConsent)
     private userConsentRepository: Repository<UserConsent>,
+    @InjectRepository(UserRole)
+    private userRoleRepository: Repository<UserRole>,
   ) {}
 
   async seed(): Promise<any> {
-    const users = DataFactory.createForClass(User).generate(5);
+    const users = DataFactory.createForClass(User).generate(500);
 
     for (const u of users) {
       const user = <User>(<unknown>u);
@@ -34,7 +38,57 @@ export class UsersSeeder implements Seeder {
       profile.user = persistedUser;
 
       this.userProfileRepository.save(profile);
+
+      //Add user role
+      await this.userRoleRepository.upsert(
+        {
+          user: { id: persistedUser.id },
+          role: 'user',
+        },
+        ['user', 'role'],
+      );
     }
+
+    //Setup admin account
+
+    const admin = new User();
+    admin.email = 'admin@revolancer.com';
+    admin.password = await argon2.hash('Password1!');
+    const persistedAdmin = await this.usersRepository.save(admin);
+
+    //Add user role
+    await this.userRoleRepository.upsert(
+      {
+        user: { id: persistedAdmin.id },
+        role: 'user',
+      },
+      ['user', 'role'],
+    );
+
+    //Add admin role
+    await this.userRoleRepository.upsert(
+      {
+        user: { id: persistedAdmin.id },
+        role: 'admin',
+      },
+      ['user', 'role'],
+    );
+
+    //Add terms consent
+    const consent = new UserConsent();
+    consent.consent_for = 'terms';
+    consent.user = persistedAdmin;
+    this.userConsentRepository.save(consent);
+
+    //Add profile
+    const profile = new UserProfile();
+    profile.first_name = 'Revolancer';
+    profile.last_name = 'Admin';
+    profile.slug = 'admin';
+    profile.profile_image =
+      'https://app.revolancer.com/img/user/avatar-placeholder.png';
+    profile.user = persistedAdmin;
+    this.userProfileRepository.save(profile);
 
     return;
   }
