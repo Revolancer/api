b23be195c1a8707d58684c3c919a21e10a23ab0c
Skye Brady <skye@revolancer.com>
2023-10-05 15:46:31 +0000

    Create index table and initial index functions

---
+++ src/features/index/entities/contentindex.entity.ts
@@ -0,0 +1,40 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  Unique,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+@Unique('content', ['otherId', 'contentType'])
+export class ContentIndex {
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
+
+  @Column('text', { default: '' })
+  otherId!: string;
+
+  @Column('text', { default: '' })
+  contentType!: string;
+
+  @Column('text', { default: '' })
+  title!: string;
+
+  @Column('text', { default: '' })
+  body!: string;
+
+  @Column('text', { array: true })
+  tagIds!: string[];
+
+  @CreateDateColumn()
+  created_at!: Date;
+
+  @UpdateDateColumn()
+  updated_at!: Date;
+
+  @DeleteDateColumn()
+  deleted_at?: Date;
+}
